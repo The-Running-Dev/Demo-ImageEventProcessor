d8e85cf62beb7a4598a65c7412a24eae9d4a9b00
Ben Richards <therunningdev@gmail.com>
2025-01-23 16:11:17 +0200

    Moved Build-Deploy to Own Script

---
+++ build-and-deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install Angular CLI
+npm install -g @angular/cli
+
+# Install Angular CLI GitHub Pages
+npm install -g angular-cli-ghpages
+
+# Install Dependencies
+npm install --prefix ImageEventUI
+
+# Build the project
+ng build --configuration production --base-href "https://the-running-dev.github.io/Demo-ImageEventProcessor/" --project ImageEventUI
+
+# Configure Git
+git config --global user.name 'github-actions[bot]'
+git config --global user.email 'github-actions[bot]@users.noreply.github.com'
+
+# Deploy to GitHub Pages
+npx angular-cli-ghpages --dir=dist/image-event-ui/browser --no-silent
